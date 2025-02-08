aaf224a27bfbf2f22fcc66b396e6e7e2778fcee8
Adam Jacobsson <adam.jacobsson@optidev.se>
2025-02-08 20:29:04 +0100

    2

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,44 +1,49 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {'saghen/blink.cmp'},
-    lazy = false,
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { 'saghen/blink.cmp' },
+        lazy = false,
+        config = function()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.ruff.setup({
-        capabilities = capabilities
-      })
+            local lspconfig = require("lspconfig")
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ruff.setup({
+                capabilities = capabilities
+            })
+            lspconfig.omnisharp.setup {
+                capabilities = capabilities,
+                cmd = { "omnisharp" },
+                root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj"),
+            }
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "gc", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
-    end,
-  },
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "gc", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
+        end,
+    },
 }
